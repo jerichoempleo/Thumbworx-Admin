cdc38fad11881983bdae1085012f60ba2f98d9e6
jerichoempleo <92818604+jerichoempleo@users.noreply.github.com>
2024-03-30 18:01:18 +0800

    User Details frontend

--- Super-Admin-Frontend/src/views/UserView.vue
+++ Super-Admin-Frontend/src/views/UserView.vue
@@ -1,6 +1,197 @@
 <template>
-	<main id="Home-page">
-		<h1>View</h1>
-		<p>This is the View page</p>
-	</main>
+    <form>
+        <fieldset>
+            <legend>Driver Details</legend>
+			<div>
+           		 <h6>Personal Information</h6>
+			</div>
+            <div class="row">
+                <div class="col mb-3">
+                    <label for="disabledTextInput1" class="form-label">First Name</label>
+                    <input type="text" id="disabledTextInput1" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput2" class="form-label">Middle Name</label>
+                    <input type="text" id="disabledTextInput2" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput3" class="form-label">Last Name</label>
+                    <input type="text" id="disabledTextInput3" class="form-control" disabled>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col mb-3">
+                    <label for="disabledTextInput4" class="form-label">Date of Birth</label>
+                    <input type="text" id="disabledTextInput4" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput5" class="form-label">Age</label>
+                    <input type="text" id="disabledTextInput5" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput6" class="form-label">Gender</label>
+                    <input type="text" id="disabledTextInput6" class="form-control" disabled>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col mb-3">
+                    <label for="disabledTextInput7" class="form-label">Marital Status</label>
+                    <input type="text" id="disabledTextInput7" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput8" class="form-label">User Type</label>
+                    <input type="text" id="disabledTextInput8" class="form-control" disabled>
+                </div>
+                <div class="col"></div> <!-- Empty column for alignment -->
+            </div>
+
+			<div>
+           		 <h6>Contact Information</h6>
+			</div>
+            <div class="row">
+                <div class="col mb-3">
+                    <label for="disabledTextInput1" class="form-label">Mobile Number</label>
+                    <input type="text" id="disabledTextInput1" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput2" class="form-label">Telephone Number</label>
+                    <input type="text" id="disabledTextInput2" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput3" class="form-label">Email</label>
+                    <input type="text" id="disabledTextInput3" class="form-control" disabled>
+                </div>
+            </div>
+
+			<div>
+           		 <h6>Address</h6>
+			</div>
+
+			<div>
+           		 <h6>Permanent Address</h6>
+			</div>
+            <div class="row">
+                <div class="col mb-3">
+                    <label for="disabledTextInput1" class="form-label">Permanent House No.</label>
+                    <input type="text" id="disabledTextInput1" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput2" class="form-label">Permanent Street</label>
+                    <input type="text" id="disabledTextInput2" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput3" class="form-label">Permanent Barangay</label>
+                    <input type="text" id="disabledTextInput3" class="form-control" disabled>
+                </div>
+            </div>
+
+			<div class="row">
+                <div class="col mb-3">
+                    <label for="disabledTextInput1" class="form-label">Permanent City.</label>
+                    <input type="text" id="disabledTextInput1" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput2" class="form-label">Permanent Province</label>
+                    <input type="text" id="disabledTextInput2" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput3" class="form-label">Permanent Region</label>
+                    <input type="text" id="disabledTextInput3" class="form-control" disabled>
+                </div>
+            </div>
+
+			<div class="row">
+                <div class="col mb-3">
+                    <label for="disabledTextInput1" class="form-label">Permanent Country.</label>
+                    <input type="text" id="disabledTextInput1" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput2" class="form-label">Permanent Zip Code</label>
+                    <input type="text" id="disabledTextInput2" class="form-control" disabled>
+                </div>
+        
+            </div>
+
+			<div>
+           		 <h6>Current Address</h6>
+			</div>
+            <div class="row">
+                <div class="col mb-3">
+                    <label for="disabledTextInput1" class="form-label">Current House No.</label>
+                    <input type="text" id="disabledTextInput1" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput2" class="form-label">Current Street</label>
+                    <input type="text" id="disabledTextInput2" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput3" class="form-label">Current Barangay</label>
+                    <input type="text" id="disabledTextInput3" class="form-control" disabled>
+                </div>
+            </div>
+
+			<div class="row">
+                <div class="col mb-3">
+                    <label for="disabledTextInput1" class="form-label">Current City.</label>
+                    <input type="text" id="disabledTextInput1" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput2" class="form-label">Current Province</label>
+                    <input type="text" id="disabledTextInput2" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput3" class="form-label">Current Region</label>
+                    <input type="text" id="disabledTextInput3" class="form-control" disabled>
+                </div>
+            </div>
+
+			<div class="row">
+                <div class="col mb-3">
+                    <label for="disabledTextInput1" class="form-label">Current Country.</label>
+                    <input type="text" id="disabledTextInput1" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput2" class="form-label">Current Zip Code</label>
+                    <input type="text" id="disabledTextInput2" class="form-control" disabled>
+                </div>
+        
+            </div>
+
+			
+			<div>
+           		 <h6>Emergency Contact Information</h6>
+			</div>
+            <div class="row">
+                <div class="col mb-3">
+                    <label for="disabledTextInput1" class="form-label">Full Name</label>
+                    <input type="text" id="disabledTextInput1" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput2" class="form-label">Relationship</label>
+                    <input type="text" id="disabledTextInput2" class="form-control" disabled>
+                </div>
+                <div class="col mb-3">
+                    <label for="disabledTextInput3" class="form-label">Mobile Number</label>
+                    <input type="text" id="disabledTextInput3" class="form-control" disabled>
+                </div>
+            </div>
+
+			<div class="row">
+                <div class="col mb-3">
+                    <label for="disabledTextInput1" class="form-label">Email</label>
+                    <input type="text" id="disabledTextInput1" class="form-control" disabled>
+                </div>
+        
+            </div>
+
+			<div>
+           		 <h6>Documents</h6>
+			</div>
+
+			
+
+            <button type="submit" class="btn btn-primary">Approve</button>
+			<button type="submit" class="btn btn-primary">Deny</button>
+        </fieldset>
+    </form>
 </template>
